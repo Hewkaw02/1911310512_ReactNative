dd157b3287b09aa99904436bdff3dde746b8224c
Hewkaw <ch.phitchaya_st@tni.ac.th>
2022-11-16 16:50:33 +0800

    "update after pratice"

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Information from "./components/Information";
-
+import styles from "./components/styles";
+import Welcome from "./components/Welcome";
+import TxtInput from "./components/Txtinput";
+import Practive1 from "./components/Practice1";
+import CustomComponent from "./components/CustomComponent";
 
 const App = () => {
   return (
     <View>
-      <Information />
+
+      <Practive1/>
+      {/* <Information /> */}
+      {/* <Welcome/>       */}
+      {/* <TxtInput/> */}
+      {/* <CustomComponent/> */}
     </View>
   );
 };
+
+
 
 export default App;
---
+++ components/Practice1.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Practice_Style from './Practice_Style'
+
+const Practice1 = () => {
+  return (
+    <View style={Practice_Style.container}>
+      <Text style={Practice_Style.title}>React Native</Text>
+    </View>
+  )
+}
+export default Practice1
