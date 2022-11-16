f13678860f0afd3d272be2763ab5d4e24d592c33
Hewkaw <ch.phitchaya_st@tni.ac.th>
2022-11-16 15:59:56 +0800

    "update before practice"

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Information from "./components/Information";
 
+
 const App = () => {
   return (
     <View>
--- components/Information.js
+++ components/Information.js
@@ -4,9 +4,19 @@ import React from 'react'
 const Information = () => {
   return (
     <View style={styles.container}>
-      <Text>React Native with Thai-Nichi</Text>
-      <Text>By.... Phitchaya Chatachot</Text>
-      <Text>Student Id: 1911310512</Text>
+      <Text style={styles.textStyle}>React Native with Thai-Nichi</Text>
+        <Text
+          style={{
+            color: "blue",
+            fontSize: 15,
+            fontWeight: "bold",
+          }}
+        >
+        By.... Phitchaya Chatachot
+        </Text>
+        <Text style={[styles.textStyle,styles.warning]}>
+          Student Id: 1911310512
+          </Text>
       <Text>Major: Computer Engineer</Text>
     </View>
   )
@@ -15,10 +25,17 @@ const Information = () => {
 export default Information
 
 const styles = StyleSheet.create({
-    container:{
-      flex:1,
-      backgroundColor:"#add8e6",
-      alignItems:"center",
-      justifyContent:'center'
-    }
-  })
+  container: {
+    flex: 1,
+    backgroundColor: "#add8e6",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    fontSize: 20,
+  },
+  warning: {
+    fontWeight: "bold",
+    color: "red",
+  },
+});
