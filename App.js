6d2293a5fba5de86d32f88face69553a144af9f8
pit_b <ch.phitchaya_st@tni.ac.th>
2023-01-25 13:24:30 +0700

    (1/25/2023) UI for next week

--- App.js
+++ App.js
@@ -1,66 +1,48 @@
-import { View, Text, ActivityIndicator, FlatList, Image } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { color } from 'react-native-reanimated';
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createDrawerNavigator,
+  DrawerItem,
+  DrawerContentScrollView,
+  DrawerItemList
+} from '@react-navigation/drawer'
+
+import HomeScreen2 from './screens/HomeScreen2'
+import ProductScreen from './screens/ProductScreen'
+
+
+function CustomDrawerContent(props) {
+  const { navigation } = props
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem label={'Close Drawer'} onPress={() => navigation.closeDrawer()} />
+    </DrawerContentScrollView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator screenOptions={{
+      drawerStyle: {
+        backgroundColor: '#fff', width: 240
+      }
+    }} useLegacyImplementation drawerContent={(props) => <CustomDrawerContent {...props} />}>
+      <Drawer.Screen name='Home' component={HomeScreen2} />
+      <Drawer.Screen name='Product' component={ProductScreen} />
+    </Drawer.Navigator>
+  )
+}
 
 const App = () => {
-
-  const [isLoading, setLoading] = useState();
-  const [data, setData] = useState([]);
-
-  const getDataNews = async () => {
-    try {
-      const response = await fetch('https://newsapi.org/v2/top-headlines?country=th&apiKey=ab0d4aca4cea481e8157d31c68eb2b23');
-      const json = await response.json();
-      setData(json.articles)
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-
-    getDataNews();
-
-  }, [])
-
-  const _renderItem = ({ item }) => {
-    return (
-      <View style={{ flex: 1 }}>
-        <View style={{ flex: 1, flexDirection: 'row', margin: 5 }}>
-          <Image resizeMode='cover'
-            source={{ uri: item.urlToImage }}
-            style={{ flex: 1, width: '100%', height: '100%' }} />
-
-          <View style={{ width: 200, margin: 5 }}>
-
-            <Text style={{ fontSize: 14, marginBottom: 5 }}>{item.title}</Text>
-            <Text style={{ fontSize: 10 }}>{item.source.name}</Text>
-            <Text style={{ fontSize: 10, color: 'red' }}>{item.publishedAt}</Text>
-          </View>
-        </View>
-      </View>
-    )
-  }
-
   return (
-    <View style={{ flex: 1, padding: 24 }}>
-      {
-        isLoading
-          ? <ActivityIndicator size="large" color="#0000ff" />
-          : (
-            <FlatList
-              style={{ margin: 6 }}
-              data={data}
-              keyExtractor={item => item.title}
-              renderItem={_renderItem}
-            />
-          )
-      }
-
-
-    </View>
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
   )
 }
 
