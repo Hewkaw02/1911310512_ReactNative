e8e99fd4cc285a3e90c434e76cb9292aa299392c
pit_b <ch.phitchaya_st@tni.ac.th>
2023-01-25 13:23:23 +0700

    (1/25/2023) UI for next week

--- App4.js
+++ App4.js
@@ -1,55 +1,57 @@
-import { View, Text , Button} from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 
 import { NavigationContainer } from '@react-navigation/native';
 import {
-createDrawerNavigator , 
-DrawerItem ,
-DrawerContentScrollView , 
-DrawerItemList
-}from '@react-navigation/drawer'
+  createDrawerNavigator,
+  DrawerItem,
+  DrawerContentScrollView,
+  DrawerItemList
+} from '@react-navigation/drawer'
 
-function Feed ({navigation}){
-  return(
-    <View style={{flex:1 , justifyContent:'center' , alignItems:'center'}}>
+function Feed({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text >Feed Screen</Text>
-      <Button title='Open Drawer' onPress={()=>navigation.openDrawer()}/>
-      <Button title='Toggle Drawer' onPress={()=>navigation.toggleDrawer()}/>
+      <Button title='Open Drawer' onPress={() => navigation.openDrawer()} />
+      <Button title='Toggle Drawer' onPress={() => navigation.toggleDrawer()} />
     </View>
   );
 }
 
-function Article (){
-  return(
-    <View style={{flex:1 , justifyContent:'center' , alignItems:'center'}}>
+function Article() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text >Article Screen</Text>
     </View>
   );
 }
 
-function CustomDrawerContent (props){
-  const {navigation} = props
-  return(
-  <DrawerContentScrollView {...props}>
-  <DrawerItemList {...props}/>
-  <DrawerItem
-  label={'Notification'}
-  onPress = {()=> alert('Link to help')}/>
-  <DrawerItem label={'Close Drawer'} onPress={()=>navigation.closeDrawer()}/>
-  <DrawerItem label={'Toggle Drawer'} onPress={()=>navigation.toggleDrawer()}/>
-  </DrawerContentScrollView>
+function CustomDrawerContent(props) {
+  const { navigation } = props
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label={'Notification'}
+        onPress={() => alert('Link to help')} />
+      <DrawerItem label={'Close Drawer'} onPress={() => navigation.closeDrawer()} />
+      <DrawerItem label={'Toggle Drawer'} onPress={() => navigation.toggleDrawer()} />
+    </DrawerContentScrollView>
   );
-  }
+}
 
 const Drawer = createDrawerNavigator();
 
-function MyDrawer(){
-  return(
-    <Drawer.Navigator screenOptions={{drawerStyle:{
-       backgroundColor:'#b3aa2f' , width:240
-    }}} useLegacyImplementation drawerContent={(props)=> <CustomDrawerContent {...props}/>}>
-        <Drawer.Screen name='Feed' component={Feed}/>
-        <Drawer.Screen name='Article' component={Article}/>
+function MyDrawer() {
+  return (
+    <Drawer.Navigator screenOptions={{
+      drawerStyle: {
+        backgroundColor: '#b3aa2f', width: 240
+      }
+    }} useLegacyImplementation drawerContent={(props) => <CustomDrawerContent {...props} />}>
+      <Drawer.Screen name='Feed' component={Feed} />
+      <Drawer.Screen name='Article' component={Article} />
     </Drawer.Navigator>
   )
 }
